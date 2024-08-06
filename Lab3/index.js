985df1f65e0ff8b7055207a3ee504c9689f7769b
Moksha Dave <mokshadave63@gmail.com>
2024-08-06 10:47:11 +0530

    innerHTML added

--- Lab3/index.js
+++ Lab3/index.js
@@ -8,3 +8,27 @@ function resetForm() {
     document.getElementById('carryout').checked = false;
 }
 
+function submitForm() {
+    var phone = document.getElementById('phone').value;
+    var size = document.getElementById('size').value;
+    var toppings = [];
+    if (document.getElementById('sausage').checked) toppings.push('Sausage');
+    if (document.getElementById('onion').checked) toppings.push('Onion');
+    if (document.getElementById('greenpepper').checked) toppings.push('Green pepper');
+    var deliveryMethod = document.querySelector('input[name="delivery"]:checked');
+
+    if (!phone || !deliveryMethod) {
+        alert("Please fill out all required fields.");
+        return false;
+    }
+
+    var orderSummary = "<h3>Your Order Summary:</h3>" +
+                       "<p>Phone Number: " + phone + "</p>" +
+                       "<p>Pizza Size: " + size + "</p>" +
+                       "<p>Toppings: " + (toppings.length > 0 ? toppings.join(', ') : "None") + "</p>" +
+                       "<p>Delivery Method: " + deliveryMethod.value + "</p>";
+
+    document.getElementById('orderSummary').innerHTML = orderSummary;
+
+    return false;  // Prevent actual form submission
+}
