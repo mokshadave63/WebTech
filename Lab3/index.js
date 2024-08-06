7f0749e61447c0a4f53714afd563dcf10db09250
Moksha Dave <mokshadave63@gmail.com>
2024-08-06 10:32:37 +0530

    Forms using HTML, CSS and basic JS

---
+++ Lab3/index.js
@@ -0,0 +1,10 @@
+function resetForm() {
+    document.getElementById('phone').value = '';
+    document.getElementById('size').selectedIndex = 0;
+    document.getElementById('sausage').checked = false;
+    document.getElementById('onion').checked = false;
+    document.getElementById('greenpepper').checked = false;
+    document.getElementById('delivery').checked = false;
+    document.getElementById('carryout').checked = false;
+}
+
